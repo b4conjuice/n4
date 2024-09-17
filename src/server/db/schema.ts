e43b5a18aff823e68d46c473fb9bb183f1ccefaf
Daniel Lopez <d@niel.to>
2024-09-17 14:40:25 -0700

    fix db table name, remove unused table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,15 +2,7 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 import { sql } from 'drizzle-orm'
-import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
-  integer,
-  bigint,
-} from 'drizzle-orm/pg-core'
+import { pgTableCreator, serial, timestamp, varchar } from 'drizzle-orm/pg-core'
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -18,34 +10,10 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator(name => `podz_${name}`)
-
-export const favorites = createTable(
-  'favorite',
-  {
-    id: serial('id').primaryKey(),
-    trackId: integer('track_id').notNull(),
-    trackName: varchar('track_name', { length: 256 }).notNull(),
-    trackViewUrl: varchar('track_view_url', { length: 1024 }).notNull(),
-    artistName: varchar('artist_name', { length: 256 }).notNull(),
-    primaryGenreName: varchar('primary_genre_name', { length: 256 }).notNull(),
-    userId: varchar('userId', { length: 256 }).notNull(),
-    createdAt: timestamp('created_at', { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  example => ({
-    nameIndex: index('name_idx').on(example.trackName),
-  })
-)
+export const createTable = pgTableCreator(name => `n4_${name}`)
 
 export const notes = createTable('note', {
   id: serial('id').primaryKey(),
-  podcastId: bigint('podcast_id', { mode: 'number' }).notNull(),
-  podcastEpisodeId: bigint('podcast_episode_id', { mode: 'number' }).notNull(),
   text: varchar('text').notNull(),
   title: varchar('title', { length: 256 }).notNull(),
   body: varchar('body').notNull(),
