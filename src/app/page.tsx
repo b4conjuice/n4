2246b852c46c80b34f41b6fc1606a9cb6caa274b
Daniel Lopez <d@niel.to>
2024-09-26 18:33:51 -0700

    update homepage to only show new note if signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+
+import TopNav from '@/app/_components/topNav'
+import { Main } from '@/components/ui'
 import Note from './notes/[id]/note'
 
 export default function Home() {
-  return <Note />
+  return (
+    <>
+      <SignedOut>
+        <TopNav />
+        <Main className='flex flex-col px-4 pb-4'>
+          <div className='flex flex-grow flex-col space-y-4'>
+            <p>sign in to create notes</p>
+          </div>
+        </Main>
+      </SignedOut>
+      <SignedIn>
+        <Note />
+      </SignedIn>
+    </>
+  )
 }
