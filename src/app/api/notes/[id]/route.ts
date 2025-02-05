6baf7f7c4e618c0ea2eca36b18cea76616ec1d37
Daniel Lopez <d@niel.to>
2025-02-04 18:31:53 -0800

    feat: add api/notes/id endpoint

---
+++ src/app/api/notes/[id]/route.ts
@@ -0,0 +1,12 @@
+import { type NextRequest } from 'next/server'
+
+import { getNote } from '@/server/queries'
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id
+  const note = await getNote(Number(id))
+  return Response.json(note)
+}
