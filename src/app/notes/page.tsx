909cbc6d3def21198a4efc3e79836ebff5db8da7
Daniel Lopez <d@niel.to>
2025-02-17 13:08:57 -0800

    feat(note-list): add footer

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,7 @@
 import type { Viewport } from 'next'
+import Link from 'next/link'
 import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { PencilSquareIcon } from '@heroicons/react/20/solid'
 
 import TopNav from '@/app/_components/topNav'
 import { Main } from '@/components/ui'
@@ -30,6 +32,17 @@ export default function NotesPage() {
           </SignedOut>
         </div>
       </Main>
+      <footer className='sticky bottom-0 flex items-center justify-between bg-cb-dusty-blue px-2 pb-4 pt-2'>
+        <div className='flex space-x-4'></div>
+        <div className='flex space-x-4'>
+          <Link
+            className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
+            href='/'
+          >
+            <PencilSquareIcon className='h-6 w-6' />
+          </Link>
+        </div>
+      </footer>
     </>
   )
 }
